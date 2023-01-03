b15a5eb094615b20163b233d8d21cfe24832ca86
장정윤 <jeinie9179@naver.com>
2023-01-03 18:03:17 +0900

    로그인 구현

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.MemberDTO;
 import com.example.demo.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
-    private MemberService memberService;
+    private final MemberService memberService;
 
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
@@ -22,6 +23,26 @@ public class MemberController {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
-        return "index";
+        return "login";
+    }
+
+    // 로그인 페이지 띄우기
+    @GetMapping("member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        System.out.println("loginResult = " + loginResult);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail()); // login 한 회원의 이메일 정보를 세션에 담는다.
+            return "main";
+        } else {
+            // login 실패
+            return "login";
+        }
     }
 }
--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -6,6 +6,8 @@ import com.example.demo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -16,5 +18,30 @@ public class MemberService {
         // 2. repository 의 save() 호출
         MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
         memberRepository.save(memberEntity);
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+        /*
+            1. 회원이 입력한 이메일로 DB 에서 조회
+            2. DB 에서 조회한 비밀번호와 사용자가 입력한 비밀번호(DTO)가 일치하는지 판단
+         */
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if (byMemberEmail.isPresent()) {
+            // 조회 결과가 있다 (해당 이메일을 가진 회원정보가 있다)
+            MemberEntity memberEntity = byMemberEmail.get(); // Optional 객체의 get() 메서드를 호출해 객체를 가져온다.
+            if (memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+                // 비밀번호 일치
+                // service 에서는 entity 객체, controller 에서는 dto 객체 사용할 예정
+                // 따라서, entity -> dto 변환 후 리턴
+                MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
+                return dto;
+            } else {
+                // 비밀번호 불일치 (로그인 실패)
+                return null;
+            }
+        } else {
+            // 조회 결과가 없다
+            return null;
+        }
     }
 }
