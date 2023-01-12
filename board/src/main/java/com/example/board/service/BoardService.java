6debdc0bc4d6d94ab1bb5286675780cbb2646da7
장정윤 <jeinie9179@naver.com>
2023-01-13 00:10:48 +0900

    게시글 조회

--- board/src/main/java/com/example/board/service/BoardService.java
+++ board/src/main/java/com/example/board/service/BoardService.java
@@ -5,9 +5,11 @@ import com.example.board.entity.BoardEntity;
 import com.example.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 // DTO -> Entity
 // Entity -> DTO
@@ -29,5 +31,21 @@ public class BoardService {
             boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
         }
         return boardDTOList;
+    }
+
+    @Transactional
+    public void updateHits(Long id) { // 조회수 증가 등의 특수한 목적을 가진 함수들은 별도의 메서드를 정의할 필요가 있음 (BoardRepository)
+        boardRepository.updateHits(id);
+    }
+
+    public BoardDTO findById(Long id) {
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(id);
+        if (optionalBoardEntity.isPresent()) {
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            BoardDTO boardDTO = BoardDTO.toBoardDTO(boardEntity);
+            return boardDTO;
+        } else {
+            return null;
+        }
     }
 }
